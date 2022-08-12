83000539ac4d34ff306a90123586a17ae88ebb7b
arefin-metatude <arefin@metatude.com>
2022-08-12 14:20:24 +0600

    test

--- jsonapp/src/main/java/com/app/jsonapp/JsonappApplication.java
+++ jsonapp/src/main/java/com/app/jsonapp/JsonappApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JsonappApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello boot!");
 		SpringApplication.run(JsonappApplication.class, args);
 	}
 
